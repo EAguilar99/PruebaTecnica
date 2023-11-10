a0a11b207f7026e86046743ec35fad6720026d49
EAguilar99 <e.agu.hndz@gmail.com>
2023-11-10 11:07:18 -0600

    Initial Commit

---
+++ PruebaTecnica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTecnica
+//
+//  Created by MacBookMBA17 on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
